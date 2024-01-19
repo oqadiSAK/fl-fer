ad19df799d25958fb13590508272a2dabb86f88c
oqadiSAK <salihardakizils@gmail.com>
2024-01-19 22:15:19 +0300

    Add FL simulation (client and driver scripts)

---
+++ simulations/fl_client.py
@@ -0,0 +1,54 @@
+import flwr as fl
+import torch
+from collections import OrderedDict
+from utils.mappers import map_eval_metrics, map_train_metrics
+from utils.training import train ,evaluate
+from utils.loaders import load_data_loaders, load_model
+
+EPOCH_PER_ROUND = 1
+LEARNING_RATE = 0.01
+MOMENTUM = 0.9
+WEIGHT_DECAY = 5e-3
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, model, device, training_loader, test_loader, val_loader):
+        self.model = model
+        self.device = device
+        self.training_loader = training_loader
+        self.test_loader = test_loader
+        self.val_loader = val_loader
+
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(self.model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        self.model.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        len, metrics = train(self.model, self.device, self.training_loader, self.val_loader,
+                            EPOCH_PER_ROUND, LEARNING_RATE, MOMENTUM, WEIGHT_DECAY)
+        
+        return self.get_parameters(config), len, map_train_metrics(metrics)
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, len, metrics = evaluate(self.model, self.device, self.test_loader)
+        return loss, len, map_eval_metrics(metrics)
+
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = load_model(device)
+    training_loader, test_loader, validation_loader = load_data_loaders()
+    
+    # Start Flower client
+    fl.client.start_numpy_client(
+        server_address="0.0.0.0:9092",  
+        client=FlowerClient(model, device, training_loader, test_loader, validation_loader),
+        transport="grpc-rere", 
+    )
+
+if __name__ == "__main__":
+    main()
---
+++ simulations/fl_driver.py
@@ -0,0 +1,72 @@
+import torch
+import flwr as fl
+from collections import OrderedDict
+from typing import List, Tuple, Dict, Optional
+from flwr.common import Metrics
+from utils.mappers import map_eval_metrics
+from utils.training import evaluate
+from utils.loaders import load_data_loaders, load_model
+
+# Define metric aggregation function
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    print(metrics)
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Multiply accuracy of each client by number of examples used
+    train_losses = [num_examples * m["train_loss"] for num_examples, m in metrics]
+    train_accuracies = [
+        num_examples * m["train_acc"] for num_examples, m in metrics
+    ]
+    val_losses = [num_examples * m["val_loss"] for num_examples, m in metrics]
+    val_accuracies = [num_examples * m["val_acc"] for num_examples, m in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {
+        "train_loss": sum(train_losses) / sum(examples),
+        "train_acc": sum(train_accuracies) / sum(examples),
+        "val_loss": sum(val_losses) / sum(examples),
+        "val_acc": sum(val_accuracies) / sum(examples),
+    }
+
+def evaluate_weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {
+        "accuracy": sum(accuracies) / sum(examples),
+    }
+
+def centralized_evaluate(
+    server_round: int,
+    parameters: fl.common.NDArrays,
+    config: Dict[str, fl.common.Scalar],
+) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+    params_dict = zip(model.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+    model.load_state_dict(state_dict, strict=True)
+    loss, _, metrics = evaluate(model, device, test_loader)
+    return loss, map_eval_metrics(metrics)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = load_model(device)
+_, test_loader, _ = load_data_loaders()
+
+# Define strategy
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1.0,  
+    min_available_clients=2,
+    fit_metrics_aggregation_fn=weighted_average,
+    evaluate_fn=centralized_evaluate,
+    evaluate_metrics_aggregation_fn=evaluate_weighted_average,
+)
+
+# Start Flower server
+history = fl.driver.start_driver(
+    server_address="0.0.0.0:9091",
+    config=fl.server.ServerConfig(num_rounds=1),
+    strategy=strategy,
+)
