731e91513ffc214f3d7345a439964ae4670ed64c
oqadiSAK <salihardakizils@gmail.com>
2024-01-20 10:04:18 +0300

    Update num_workers parameter in load_data_loaders function

--- simulations/utils/loaders.py
+++ simulations/utils/loaders.py
@@ -5,7 +5,7 @@ from utils.dataset_factory import DataSetFactory
 SHAPE = (44, 44)
 CLASSES = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-def load_data_loaders(batch_size=128, num_workers=2):
+def load_data_loaders(batch_size=128, num_workers=0):
     factory = DataSetFactory(SHAPE)
     training_loader = DataLoader(factory.training, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     test_loader = DataLoader(factory.public, batch_size=batch_size, shuffle=True, num_workers=num_workers)
