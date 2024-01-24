f9473e809364392b116876b055ee50d1e070ce37
Salih Arda Kizil <salihardakizils@gmail.com>
2024-01-25 01:57:05 +0300

    Refactor custom strategy

--- strategy.py
+++ strategy.py
@@ -2,8 +2,7 @@ import flwr as fl
 import numpy as np
 from functools import reduce
 from logging import WARNING
-from typing import Dict, List, Optional, Tuple, Union
-
+from typing import Dict, List, Optional, Tuple, Union, override
 from flwr.common import (
     FitRes,
     EvaluateRes,
@@ -17,57 +16,37 @@ from flwr.common.logger import log
 from flwr.server.client_proxy import ClientProxy
 
 class CustomFedAvg(fl.server.strategy.FedAvg):
+    
+    @override
     def aggregate_fit(
         self,
         server_round: int,
         results: List[Tuple[ClientProxy, FitRes]],
         failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
     ) -> Tuple[Optional[Parameters], Dict[str, Scalar]]:
-        """Aggregate fit results using weighted average."""
         if not results:
             return None, {}
-        # Do not aggregate if there are failures and failures are not accepted
+        
         if not self.accept_failures and failures:
             return None, {}
 
-        # Convert results
         weights_results = [
             (parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples)
             for _, fit_res in results
         ]
-        parameters_aggregated = ndarrays_to_parameters(self.custom_aggregate(weights_results))
+        parameters_aggregated = ndarrays_to_parameters(self._custom_aggregate(weights_results))
 
-        # Aggregate custom metrics if aggregation fn was provided
         metrics_aggregated = {}
         if self.fit_metrics_aggregation_fn:
             fit_metrics = [(res.num_examples, res.metrics) for _, res in results]
             metrics_aggregated = self.fit_metrics_aggregation_fn(fit_metrics)
-        elif server_round == 1:  # Only log this warning once
+        elif server_round == 1: 
             log(WARNING, "No fit_metrics_aggregation_fn provided")
 
         return parameters_aggregated, metrics_aggregated
     
-    def custom_aggregate(self, results: List[Tuple[NDArrays, int]]) -> NDArrays:
-        """Compute weighted average."""
-        # Calculate the total number of examples used during training
-        num_examples_total = sum(num_examples for (_, num_examples) in results)
-
-        # If no examples were used in training, return the weights from the first result
-        if num_examples_total == 0:
-            return results[0][0]
-
-        # Create a list of weights, each multiplied by the related number of examples
-        weighted_weights = [
-            [layer * num_examples for layer in weights] if num_examples != 0 else [0]*len(weights) for weights, num_examples in results
-        ]
-
-        # Compute average weights of each layer
-        weights_prime: NDArrays = [
-            reduce(np.add, layer_updates) / num_examples_total
-            for layer_updates in zip(*weighted_weights)
-        ]
-        return weights_prime
-    
+        
+    @override
     def aggregate_evaluate(
         self,
         server_round: int,
@@ -75,4 +54,19 @@ class CustomFedAvg(fl.server.strategy.FedAvg):
         failures: List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]],
     ) -> Tuple[Optional[float], Dict[str, Scalar]]:
         return None, {}
+    
+    def _custom_aggregate(self, results: List[Tuple[NDArrays, int]]) -> NDArrays:
+        num_examples_total = sum(num_examples for (_, num_examples) in results)
+        if num_examples_total == 0:
+            return results[0][0]
+
+        weighted_weights = [
+            [layer * num_examples for layer in weights] if num_examples != 0 else [0]*len(weights) for weights, num_examples in results
+        ]
+
+        weights_prime: NDArrays = [
+            reduce(np.add, layer_updates) / num_examples_total
+            for layer_updates in zip(*weighted_weights)
+        ]
+        return weights_prime
        
