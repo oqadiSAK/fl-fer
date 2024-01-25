41d1c70e4e1c4260fb8011f6b97cbc05f4d74258
Salih Arda Kizil <salihardakizils@gmail.com>
2024-01-25 03:43:16 +0300

    Reverted VGG19 model since pi is not enough

--- model.py
+++ model.py
@@ -1,31 +1,83 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
-cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+class SeparableConv2d(nn.Module):
 
-class VGG19(nn.Module):
-    def __init__(self, num_classes=7):
-        super(VGG19, self).__init__()
-        self.features = self._make_layers(cfg)
-        self.classifier = nn.Linear(512, num_classes)
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, bias=False):
+        super(SeparableConv2d, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels,
+                                   bias=bias)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, 1, 1, 0, 1, 1, bias=bias)
 
     def forward(self, x):
-        out = self.features(x)
-        out = out.view(out.size(0), -1)
-        out = F.dropout(out, p=0.5, training=self.training)
-        out = self.classifier(out)
-        return out
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
 
-    def _make_layers(self, cfg):
-        layers = []
-        in_channels = 1
-        for x in cfg:
-            if x == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            else:
-                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                           nn.BatchNorm2d(x),
-                           nn.ReLU(inplace=True)]
-                in_channels = x
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
-        return nn.Sequential(*layers)
+
+class ResidualBlock(nn.Module):
+
+    def __init__(self, in_channeld, out_channels):
+        super(ResidualBlock, self).__init__()
+
+        self.residual_conv = nn.Conv2d(in_channels=in_channeld, out_channels=out_channels, kernel_size=1, stride=2,
+                                       bias=False)
+        self.residual_bn = nn.BatchNorm2d(out_channels, momentum=0.99, eps=1e-3)
+
+        self.sepConv1 = SeparableConv2d(in_channels=in_channeld, out_channels=out_channels, kernel_size=3, bias=False,
+                                        padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels, momentum=0.99, eps=1e-3)
+        self.relu = nn.ReLU()
+
+        self.sepConv2 = SeparableConv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, bias=False,
+                                        padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels, momentum=0.99, eps=1e-3)
+        self.maxp = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        res = self.residual_conv(x)
+        res = self.residual_bn(res)
+        x = self.sepConv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.sepConv2(x)
+        x = self.bn2(x)
+        x = self.maxp(x)
+        return res + x
+
+
+class Model(nn.Module):
+
+    def __init__(self, num_classes):
+        super(Model, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(8, affine=True, momentum=0.99, eps=1e-3)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(8, momentum=0.99, eps=1e-3)
+        self.relu2 = nn.ReLU()
+
+        self.module1 = ResidualBlock(in_channeld=8, out_channels=16)
+        self.module2 = ResidualBlock(in_channeld=16, out_channels=32)
+        self.module3 = ResidualBlock(in_channeld=32, out_channels=64)
+        self.module4 = ResidualBlock(in_channeld=64, out_channels=128)
+
+        self.last_conv = nn.Conv2d(in_channels=128, out_channels=num_classes, kernel_size=3, padding=1)
+        self.avgp = nn.AdaptiveAvgPool2d((1, 1))
+
+    def forward(self, input):
+        x = input
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.module1(x)
+        x = self.module2(x)
+        x = self.module3(x)
+        x = self.module4(x)
+        x = self.last_conv(x)
+        x = self.avgp(x)
+        x = x.view((x.shape[0], -1))
+        return x
